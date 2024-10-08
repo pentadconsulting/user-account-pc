bff4e6f67f84fe8293ccea7b631e4256ddf90ff9
Shailesh Kumar <shailesh.k.pentadcs@outlook.com>
2024-10-08 10:13:14 +0530

    page

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ import axios from 'axios';
 
 
 export async function signup (url, username, password){
-    try {
-        console.log("i am in sign up");
+    try{
         const response = await axios.post(url, {
-            userName: username,
-            password: password,
-            
-        });
-        
-        if (!response.ok) {
-             throw new Error('Network response was not ok');
-        }
-        
-        return response.data;
-    } 
+        userName: username,
+        password: password,
+      }, {
+        headers: {
+          'accept': '*/*',
+          'Content-Type': 'application/json',
+        },
+      });
+      
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+    }
     catch (error) {
         console.error(error);
     }
